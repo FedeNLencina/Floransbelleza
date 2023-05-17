dbee96a066e50e28312d5052945fa07edbf17fd4
Federico Lencina <federiconahuellencina@gmail.com>
2023-05-16 22:55:47 -0300

    refactor: 💡 Add Home component and insert hero there

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
 import About from "./components/about/About";
-import { Hero } from "./components/hero/Hero";
+import { Home } from "./components/home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Hero/>}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Hero } from "../hero/Hero";
+
+export function Home() {
+  return <Hero />;
+}
