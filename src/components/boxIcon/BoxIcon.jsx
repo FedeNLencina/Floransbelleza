8cd7d0a1335aaf9e86802170bd10e07d65d72053
Federico Lencina <federiconahuellencina@gmail.com>
2023-05-23 00:00:37 -0300

    feat: 🎸 add iconBox component

---
+++ src/components/boxIcon/BoxIcon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function BoxIcon(props) {
+  return (
+    <div className="container-fluid">
+      <div className="flex-column justify-content-center align-items-center"></div>
+    </div>
+  );
+}
--- src/components/serviceHome/ServiceHome.jsx
+++ src/components/serviceHome/ServiceHome.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function ServiceHome() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="row"></div>
+    </div>
+  );
 }
