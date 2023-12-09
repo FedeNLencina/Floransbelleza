9196f8037e917888a8d68f9210b5ffa48ddb3100
Federico Lencina <federiconahuellencina@gmail.com>
2023-12-09 19:22:50 -0300

    refactor: 💡 testimonials and services container style

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./testimonials.css";
 import { testimonials } from "../../services/testimonials";
+import { Title } from "../commons/Title/Title";
 
 export function Testimonials() {
   return (
     <div class="testimonialsContainer">
+      <div class="text-center testimonialsTitleContainer">
+        <h1 class="testimonialsTitle">Nuestros testimionios</h1>
+      </div>
       <div class="container">
         <div id="demo" class="carousel slide" data-bs-ride="true">
           <div class="carousel-inner">
@@ -22,40 +26,6 @@ export function Testimonials() {
                 </div>
               );
             })}
-            {/* <div class="carousel-caption">
-              <p>
-                If Shai Reznik's TDD videos don't convince you to add automated
-                testing your code, I don't know what will.This was the very best
-                explanation of frameworks for brginners that I've ever seen.
-              </p>
-              <img src="https://i.imgur.com/lE89Aey.jpg" />
-              <div id="image-caption">Nick Doe</div>
-            </div> */}
-
-            {/* <div class="carousel-item ">
-              <div class="carousel-caption">
-                <p>
-                  If Shai Reznik's TDD videos don't convince you to add
-                  automated testing your code, I don't know what will.This was
-                  the very best explanation of frameworks for brginners that
-                  I've ever seen.
-                </p>
-                <img src="https://i.imgur.com/QptVdsp.jpg" class="img-fluid" />
-                <div id="image-caption">Cromption Greves</div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <div class="carousel-caption">
-                <p>
-                  If Shai Reznik's TDD videos don't convince you to add
-                  automated testing your code, I don't know what will.This was
-                  the very best explanation of frameworks for brginners that
-                  I've ever seen.
-                </p>
-                <img src="https://i.imgur.com/jQWThIn.jpg" class="img-fluid" />
-                <div id="image-caption">Harry Mon</div>
-              </div>
-            </div> */}
           </div>
           <a
             class="carousel-control-prev"
