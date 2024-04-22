7b214340e8baed11f5dacc676ed6623014f36916
Federico Lencina <federiconahuellencina@gmail.com>
2024-04-21 23:05:53 -0300

    fix: 🐛 fix link free widget from page

--- src/components/home/instagramFeed/InstagramFeed.tsx
+++ src/components/home/instagramFeed/InstagramFeed.tsx
@@ -2,21 +2,29 @@ import React, { useEffect } from "react";
 
 export function InstagramFeed() {
   useEffect(() => {
-    // Función para eliminar el enlace con el texto específico
-    const eliminarEnlace = () => {
-      const enlaces = document.querySelectorAll(".instaLayout a"); // Seleccionar todos los enlaces dentro de .instaLayout
-      enlaces.forEach((enlace) => {
-        if (enlace.textContent.trim() === "Free Instagram Feed widget") {
-          // Verificar si el texto del enlace coincide
-          enlace.parentNode.removeChild(enlace); // Eliminar el enlace
+    const observerCallback = (mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          mutation.addedNodes.forEach((node) => {
+            if (
+              node.tagName === "A" &&
+              node.textContent.trim() === "Free Instagram Feed widget"
+            ) {
+              node.parentNode.removeChild(node);
+              observer.disconnect();
+            }
+          });
         }
-      });
+      }
     };
 
-    // Llamar a la función para eliminar el enlace al montar el componente
-    eliminarEnlace();
+    const observer = new MutationObserver(observerCallback);
 
-    // No necesitamos limpiar ningún listener aquí ya que esta función solo se ejecuta una vez
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    return () => {
+      observer.disconnect();
+    };
   }, []);
 
   return (
