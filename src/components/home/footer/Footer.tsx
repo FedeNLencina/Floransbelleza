a5fa8bedfacd3669281786a4d1e33d21f16f0ee5
Federico Lencina <federiconahuellencina@gmail.com>
2024-04-23 23:06:35 -0300

    feat: 🎸 add footer home

---
+++ src/components/home/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6 col-lg-3"></div>
+        <div className="col-6 col-lg-3"></div>
+        <div className="col-6 col-lg-3"></div>
+        <div className="col-6 col-lg-3"></div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="">© 2022 Floransbelleza. All Rights Reserved</div>
+        <div className="">Privacy Policy Terms & Conditions</div>
+      </div>
+    </div>
+  );
+}
