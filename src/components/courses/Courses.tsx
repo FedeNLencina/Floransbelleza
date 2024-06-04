93535e12c47522302d4a6bc240a17e8156f6ec07
Federico <federiconahuellencina@gmail.com>
2024-06-03 21:57:03 -0300

    feat: 🎸 add cards mocks and their styles

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import "./Courses.css";
 
 export const Courses = () => {
-    return (
-        <div className="container" style={{ height: '100vh' }}>
-            <div className='row h-100 align-items-center'>
+  return (
+    <div className="container" style={{ height: "100vh" }}>
+      {/* <div className='row h-100 align-items-center'>
                 <div className='col-lg-4 col-12 cardLinkCourseContainer text-center '>
                     <h2>Cursos Online</h2>
                 </div>
@@ -16,7 +17,30 @@ export const Courses = () => {
                 </div>
 
 
-            </div>
+            </div> */}
+      <div className="row h-100 align-items-center">
+        <div className="ag-courses_item col-lg-4 col-12 text-center">
+          <a href="#" className="ag-courses-item_link">
+            <div className="ag-courses-item_bg"></div>
+
+            <div className="ag-courses-item_title">Cursos online</div>
+          </a>
         </div>
-    )
-}
+        <div className="ag-courses_item col-lg-4 col-12 text-center">
+          <a href="#" className="ag-courses-item_link">
+            <div className="ag-courses-item_bg"></div>
+
+            <div className="ag-courses-item_title">Cursos online</div>
+          </a>
+        </div>
+        <div className="ag-courses_item col-lg-4 col-12 text-center">
+          <a href="#" className="ag-courses-item_link">
+            <div className="ag-courses-item_bg"></div>
+
+            <div className="ag-courses-item_title">Cursos online</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
