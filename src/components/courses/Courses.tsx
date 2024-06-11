8364372bc204c866f263176091cbcbcb408123d2
Federico <federiconahuellencina@gmail.com>
2024-06-11 13:18:03 -0300

    refactor: 💡 delete comments

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -4,20 +4,6 @@ import "./Courses.css";
 export const Courses = () => {
   return (
     <div className="container" style={{ height: "100vh" }}>
-      {/* <div className='row h-100 align-items-center'>
-                <div className='col-lg-4 col-12 cardLinkCourseContainer text-center '>
-                    <h2>Cursos Online</h2>
-                </div>
-                <div className='col-lg-4 col-12 cardLinkCourseContainer text-center'>
-                    <h2>Cursos Online</h2>
-                </div>
-
-                <div className='col-lg-4 col-12 cardLinkCourseContainer text-center'>
-                    <h2>Cursos Online</h2>
-                </div>
-
-
-            </div> */}
       <div className="row h-100 align-items-center">
         <div className="ag-courses_item col-lg-4 col-12 text-center">
           <a href="#" className="ag-courses-item_link">
