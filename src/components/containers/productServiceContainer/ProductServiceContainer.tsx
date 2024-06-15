833cdab50f9365a08975f0a58ca24f41a2592b65
Federico Lencina <federiconahuellencina@gmail.com>
2024-06-15 16:35:49 -0300

    feat: 🎸 add integration with firebase

--- src/components/containers/productServiceContainer/ProductServiceContainer.tsx
+++ src/components/containers/productServiceContainer/ProductServiceContainer.tsx
@@ -1,11 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { productServices } from "../../../utils/productServices";
 import { ProductServiceList } from "../../lists/productServiceList/ProductServiceList";
 import { ServiceListItem } from "src/types/ServiceListItem";
-export const ProductServiceContainer = () => {
-  const [servicesArray, setServicesArray] =
-    useState<Array<ServiceListItem>>(productServices);
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "src/firebase/config.database";
 
+export const ProductServiceContainer = () => {
+  const [servicesArray, setServicesArray] = useState<Array<ServiceListItem>>(
+    []
+  );
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const photoGalleryRef = await getDocs(
+          collection(db, "servicesGallery")
+        );
+        const newArray: ServiceListItem[] = [];
+
+        photoGalleryRef.forEach((document) => {
+          const documentRef = document.data();
+          const photoGalleryObject: ServiceListItem = {
+            id: documentRef.id,
+            title: documentRef.title,
+            imagePath: documentRef.image,
+            description: "",
+          };
+          newArray.push(photoGalleryObject);
+        });
+        setServicesArray(newArray);
+      } catch (e) {
+        console.log("Error getting cached document:", e);
+      }
+    };
+
+    getData();
+  }, []);
   return (
     <div className="container">
       <ProductServiceList services={servicesArray} />
