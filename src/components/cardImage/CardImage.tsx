c9e198a09d8951b55a09622e6e3d76e59e000b0b
Federico Lencina <federiconahuellencina@gmail.com>
2024-06-22 01:06:45 -0300

    feat: 🎸 add img modal mock

--- src/components/cardImage/CardImage.tsx
+++ src/components/cardImage/CardImage.tsx
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./cardImage.css";
 import { ServiceListItem } from "src/types/ServiceListItem";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import Skeleton from "react-loading-skeleton";
-import { Loader } from "@components/loader/Loader";
+import { ModalImage } from "./modalImage/ModalImage";
 
 type CardImageProps = {
   element: ServiceListItem;
 };
 
 export const CardImage = ({ element }: CardImageProps) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  useEffect(() => {}, [showModal]);
   return (
     <>
       <div
         className="col-sm-6 col-md-6 col-lg-4 cardCol d-flex justify-content-center aling-items-center"
         key={element.id}
       >
-        <div className="card">
+        <div
+          className="card"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          onClick={openModal}
+        >
           <LazyLoadImage
             src={element.imagePath}
             className="card-img-top img-fluid cardImg"
             alt="..."
           />
+
           <figcaption>
             <h3>
               <span>{element.title}</span>
             </h3>
           </figcaption>
         </div>
+        {showModal && <ModalImage imgPath={element.imagePath} />}
       </div>
     </>
   );
