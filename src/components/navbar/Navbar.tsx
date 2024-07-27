e9b1ed898edb4dd894eb060b2c72be8f4eb5c21c
Federico Lencina <federiconahuellencina@gmail.com>
2024-07-27 18:38:17 -0300

    style: 💄 chage padding for back image

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -49,129 +49,133 @@ export function Navbar() {
           : "navInactive"
       }`}
     >
-      <div className="container-fluid divNav ">
-        <img src={Logo} alt="logo" className="logo" />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="offcanvas offcanvas-end divLinks"
-          tabIndex={-1}
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
-          <div className="offcanvas-header">
-            <img
-              src={Logo}
-              alt="logo"
-              className="logo offcanvas-title"
-              id="offcanvasNavbarLabel"
-            />
-            <button
-              type="button"
-              className="btn-close offcanvas-title"
-              id="offcanvasNavbarLabel"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="divNavInnerContainer d-flex justify-content-center align-items-center">
+          <div className="logoContainer">
+            <img src={Logo} alt="logo" className="logo" />
           </div>
-          <div className="offcanvas-body linksContainer">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li
-                className="nav-item"
-                aria-current="page"
-                data-bs-toggle="offcanvas"
-              >
-                <Link
-                  to="/"
-                  className={`navLink ${
-                    (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
-                    isNotAboutPathAndIsActive
-                      ? "linkActive"
-                      : "linkInactive"
-                  }`}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end divLinks"
+            tabIndex={-1}
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <img
+                src={Logo}
+                alt="logo"
+                className="logo offcanvas-title"
+                id="offcanvasNavbarLabel"
+              />
+              <button
+                type="button"
+                className="btn-close offcanvas-title"
+                id="offcanvasNavbarLabel"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body linksContainer">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li
+                  className="nav-item"
+                  aria-current="page"
+                  data-bs-toggle="offcanvas"
                 >
-                  Home
-                </Link>
-              </li>
+                  <Link
+                    to="/"
+                    className={`navLink ${
+                      (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
+                      isNotAboutPathAndIsActive
+                        ? "linkActive"
+                        : "linkInactive"
+                    }`}
+                  >
+                    Home
+                  </Link>
+                </li>
 
-              <li
-                className="nav-item"
-                aria-current="page"
-                data-bs-toggle="offcanvas"
-              >
-                <Link
-                  to="/about"
-                  className={`navLink ${
-                    (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
-                    isNotAboutPathAndIsActive
-                      ? "linkActive"
-                      : "linkInactive"
-                  }`}
+                <li
+                  className="nav-item"
+                  aria-current="page"
+                  data-bs-toggle="offcanvas"
                 >
-                  About
-                </Link>
-              </li>
+                  <Link
+                    to="/about"
+                    className={`navLink ${
+                      (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
+                      isNotAboutPathAndIsActive
+                        ? "linkActive"
+                        : "linkInactive"
+                    }`}
+                  >
+                    About
+                  </Link>
+                </li>
 
-              <li
-                className="nav-item"
-                aria-current="page"
-                data-bs-toggle="offcanvas"
-              >
-                <Link
-                  to="/servicios"
-                  className={`navLink ${
-                    (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
-                    isNotAboutPathAndIsActive
-                      ? "linkActive"
-                      : "linkInactive"
-                  }`}
+                <li
+                  className="nav-item"
+                  aria-current="page"
+                  data-bs-toggle="offcanvas"
                 >
-                  Servicios
-                </Link>
-              </li>
-              <li
-                className="nav-item"
-                aria-current="page"
-                data-bs-toggle="offcanvas"
-              >
-                <Link
-                  to="/talleres"
-                  className={`navLink ${
-                    (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
-                    isNotAboutPathAndIsActive
-                      ? "linkActive"
-                      : "linkInactive"
-                  }`}
+                  <Link
+                    to="/servicios"
+                    className={`navLink ${
+                      (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
+                      isNotAboutPathAndIsActive
+                        ? "linkActive"
+                        : "linkInactive"
+                    }`}
+                  >
+                    Servicios
+                  </Link>
+                </li>
+                <li
+                  className="nav-item"
+                  aria-current="page"
+                  data-bs-toggle="offcanvas"
                 >
-                  Talleres
-                </Link>
-              </li>
-              <li
-                className="nav-item"
-                aria-current="page"
-                data-bs-toggle="offcanvas"
-              >
-                <Link
-                  to="/contacto"
-                  className={`navLink ${
-                    (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
-                    isNotAboutPathAndIsActive
-                      ? "linkActive"
-                      : "linkInactive"
-                  }`}
+                  <Link
+                    to="/talleres"
+                    className={`navLink ${
+                      (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
+                      isNotAboutPathAndIsActive
+                        ? "linkActive"
+                        : "linkInactive"
+                    }`}
+                  >
+                    Talleres
+                  </Link>
+                </li>
+                <li
+                  className="nav-item"
+                  aria-current="page"
+                  data-bs-toggle="offcanvas"
                 >
-                  Contacto
-                </Link>
-              </li>
-            </ul>
+                  <Link
+                    to="/contacto"
+                    className={`navLink ${
+                      (isAboutPathAndIsActive && !isAboutPathAndIsInactive) ||
+                      isNotAboutPathAndIsActive
+                        ? "linkActive"
+                        : "linkInactive"
+                    }`}
+                  >
+                    Contacto
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
