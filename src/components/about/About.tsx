42bcf7446a7b2463b65d19001217d9c4d716dc29
Federico <federiconahuellencina@gmail.com>
2024-08-06 14:57:02 -0300

    feat: 🎸 add container to about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -11,6 +11,12 @@ export default function About() {
           <h2 className="topSectionText">Conocenos</h2>
         </div>
       </section>
+      <div className="container">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </div>
     </div>
   );
 }
