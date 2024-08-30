fe870863504f4baa4db5b07b140841127468dac9
Federico Lencina <federiconahuellencina@gmail.com>
2024-08-29 23:08:30 -0300

    refactor: 💡 separates footer and testimonials component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,11 +3,11 @@ import { Hero } from "./hero/Hero";
 import { AboutHome } from "./aboutHome/AboutHome";
 import { ServiceHome } from "./serviceHome/ServiceHome";
 import { ServiceHomeProvider } from "../../context/ServiceHomeIconContext";
-import { Testimonials } from "./testimonials/Testimonials";
+import { Testimonials } from "../testimonials/Testimonials";
 import { Booking } from "./booking/Booking";
 import { PictureGalery } from "./pictureGallery/PictureGalery";
 import { InstagramFeed } from "./instagramFeed/InstagramFeed";
-import { Footer } from "./footer/Footer";
+import { Footer } from "../footer/Footer";
 import { ScrollTopButton } from "./scrollTopButton/ScrollTopButton";
 
 export function Home() {
