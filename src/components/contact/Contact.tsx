7c0d98e59364908abb99e2f15b90bdb7b9c1e909
Federico Lencina <federiconahuellencina@gmail.com>
2024-09-06 00:20:36 -0300

    feat: 🎸 add contact component and style

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import firstSectionImage from "../../assets/images/about/Screenshot_307-min.png";
+import "./contact.css";
+import { ScrollTopButton } from "@components/home/scrollTopButton/ScrollTopButton";
+
+export default function Contact() {
+  return (
+    <div className="container-fluid contactContainer">
+      <section className="topSection">
+        <div className="topSectionBlackBackground"></div>
+        <div className="topSectionInfoContainer">
+          <h1 className="topSectionTitle">Comenza tu cambio!</h1>
+          <h2 className="topSectionText">Conocenos</h2>
+        </div>
+      </section>
+      <div className="container contactFirstSection">
+        <div className="row">
+          <div className="col-12 col-lg-6"></div>
+          <div className="col-12 col-lg-6 "></div>
+        </div>
+      </div>
+      <ScrollTopButton offset={200} />
+    </div>
+  );
+}
